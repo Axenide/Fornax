f987344538bb323cc04c66ad742280130e7db5da
term <term@local>
2026-06-10 20:41:47 -0300

    term: drop neofetch-derived aliases

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,9 +1,3 @@
-# Custom fastfetch
-alias axfetch 'clear && fastfetch'
-alias kittyfetch 'clear && fastfetch'
-alias sansfetch 'clear && fastfetch'
-alias adrienfetch 'clear && fastfetch'
 alias anifetch 'kitty +kitten icat -n --place 100x8@0x0 --align left ~/.adrien.gif | fastfetch -c minimal --logo-width 25 --raw -'
 
-# Cava gradient support for Tmux
 alias cavax 'TERM=st-256color cava'
